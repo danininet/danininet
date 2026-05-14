4d9db1280ac15720bf6ccaf49754255b007ae117
Dragan Zdravkovic <127017058+danininet@users.noreply.github.com>
2026-05-15 01:13:58 +0200

    Add Plasmic loader configuration

---
+++ lib/plasmic.ts
@@ -0,0 +1,18 @@
+import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+
+const projectId = process.env.PLASMIC_PROJECT_ID;
+const projectToken = process.env.PLASMIC_PROJECT_TOKEN;
+
+export const hasPlasmicConfig = Boolean(projectId && projectToken);
+
+export const PLASMIC = hasPlasmicConfig
+  ? initPlasmicLoader({
+      projects: [
+        {
+          id: projectId!,
+          token: projectToken!,
+        },
+      ],
+      preview: process.env.PLASMIC_PREVIEW === "true",
+    })
+  : null;
