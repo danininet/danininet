f637de6c5540350274cc7115c3dc4607f13b6324
Dragan Zdravkovic <127017058+danininet@users.noreply.github.com>
2026-05-15 21:32:01 +0200

    Expose Plasmic env values for client renderer

--- lib/plasmic.ts
+++ lib/plasmic.ts
@@ -1,18 +1,19 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs/react-server";
 
-const projectId = process.env.PLASMIC_PROJECT_ID;
-const projectToken = process.env.PLASMIC_PROJECT_TOKEN;
+export const PLASMIC_PROJECT_ID = process.env.PLASMIC_PROJECT_ID || "";
+export const PLASMIC_PROJECT_TOKEN = process.env.PLASMIC_PROJECT_TOKEN || "";
+export const PLASMIC_PREVIEW = process.env.PLASMIC_PREVIEW === "true";
 
-export const hasPlasmicConfig = Boolean(projectId && projectToken);
+export const hasPlasmicConfig = Boolean(PLASMIC_PROJECT_ID && PLASMIC_PROJECT_TOKEN);
 
 export const PLASMIC = hasPlasmicConfig
   ? initPlasmicLoader({
       projects: [
         {
-          id: projectId!,
-          token: projectToken!,
+          id: PLASMIC_PROJECT_ID,
+          token: PLASMIC_PROJECT_TOKEN,
         },
       ],
-      preview: process.env.PLASMIC_PREVIEW === "true",
+      preview: PLASMIC_PREVIEW,
     })
   : null;
