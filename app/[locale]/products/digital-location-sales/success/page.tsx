79b8dc513b1b9c7b846cb4337c7384e33c394ed1
Dragan Zdravkovic <127017058+danininet@users.noreply.github.com>
2026-05-25 21:59:05 +0200

    Fix EN DPL success alias import path

--- app/[locale]/products/digital-location-sales/success/page.tsx
+++ app/[locale]/products/digital-location-sales/success/page.tsx
@@ -1,3 +1,3 @@
-import SuccessPage from "../../../../proizvodi/digitalna-prodaja-lokacije/uspeh/page";
+import SuccessPage from "../../../proizvodi/digitalna-prodaja-lokacije/uspeh/page";
 
 export default SuccessPage;
